89ee01a21a56941c33f1fd5a18d31a4c43203e7d
Scumworth <danmcninch@gmail.com>
2019-05-28 18:15:13 -0400

    adds spacemacs

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc eslintrc tmux.conf zshrc"    # list of files/folders to symlink in homedir
+files="bashrc vimrc eslintrc tmux.conf zshrc spacemacs"    # list of files/folders to symlink in homedir
 
 ##########
 
