bfafbfdd935330f09224a0ccebb96f1e078a77c2
Daniel McNinch <danmcninch@gmail.com>
2022-04-17 21:54:14 -0700

    Updates script to install dotfiles

---
+++ linux/plugins.vim
@@ -0,0 +1,24 @@
+call plug#begin('~/.vim/plugged')
+Plug 'scrooloose/nerdtree'
+Plug 'tpope/vim-fugitive'
+Plug 'itchyny/lightline.vim'
+Plug 'christoomey/vim-tmux-navigator'
+Plug 'cocopon/iceberg.vim'
+Plug 'gkeep/iceberg-dark'
+Plug 'olical/conjure'
+Plug 'wlangstroth/vim-racket'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'TimUntersberger/neogit'
+Plug 'burntsushi/erd'
+Plug 'junegunn/fzf', {'do': { -> fzf#install() }}
+Plug 'junegunn/fzf.vim'
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'vimwiki/vimwiki'
+Plug 'scrooloose/nerdcommenter'
+Plug 'luochen1990/rainbow'
+Plug 'tpope/vim-surround'
+Plug 'mileszs/ack.vim'
+Plug 'xuyuanp/nerdtree-git-plugin'
+Plug 'epmatsw/ag.vim'
+Plug 'kassio/neoterm'
+call plug#end()
--- setup.sh
+++ setup.sh
@@ -6,70 +6,86 @@
 # sets up oh-my-zsh
 ############################
 
-########## Variables
+############################
+######## Variables #########
+############################
 
-# check distro
 uname_out="$(uname -s)"
-if [ $uname_out == "Darwin" ] then
+if [ $uname_out == "Darwin" ]; then
+    echo "Platform is mac."
     platform="mac"
-elif [ $uname_out == "Linux" ] then
+elif [ $uname_out == "Linux" ]; then
+    echo "Platform is linux."
     platform="linux"
 else
     echo "No appropriate platform found. Exiting."
     exit 1
 fi
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+new_dir="$HOME/repos/dotfiles/$platform"       # dotfiles directory
 
-home_dir="~"
 home_files="bashrc vimrc tmux.conf zshrc"    # list of files/folders to symlink in homedir
 
-vim_plugin_dir="~/.vim
-vim_plugin_files"plugins.vim"
+vim_plugin_dir="$HOME/.vim"
+vim_plugin_files="plugins.vim"
 
-nvim_dir="~/.config/nvim"
+nvim_dir="$HOME/.config/nvim"
 nvim_files="coc-settings.json init.vim" 
 
-alacritty_dir="~/.config/alacritty"
-alacritty_files = "alacritty.yml"
+alacritty_dir="$HOME/.config/alacritty"
+alacritty_files="alacritty.yml"
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+############################
+####### Directories ########
+############################
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in home directory to dotfiles_old directory, then create symlinks 
 for file in $home_files; do
-    echo "Moving any existing home dotfiles from $home_dir to $olddir"
-    mv $home_dir/.$file $home_dir/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file $home_dir/.$file
+    if [ -L "${HOME}/.${file}" ]; then
+        echo "Removing existing symlink ${file}"
+        rm $HOME/.$file 
+    fi
+    echo "Creating new symlink to ${file} in home directory."
+    echo $new_dir/$file
+    ln -s $new_dir/$file $HOME/.$file
 done
+echo "...done"
 
 for file in $vim_plugin_files; do
-    echo "Moving any existing vim plugin dotfiles from $vim_plugin_dir to $olddir"
-    mv $vim_plugin_dir/$file $home_dir/dotfiles_old
+    if [ -L $vim_plugin_dir/$file ]; then
+        echo "Removing Moving existing symlink ${file}"
+        rm $vim_plugin_dir/$file
+    fi
+    echo "Creating new symlink to $file in vim plugins directory."
+    ln -s $new_dir/$file $vim_plugin_dir/$file
 done
+echo "...done"
 
 for file in $nvim_files; do
-    echo "Moving any existing nvim files from $nvim_dir to $olddir"
+    if [ -L $nvim_dir/$file ]; then
+        echo "Removing existing symlink ${file} from ${nvim_dir}"
+        rm $nvim_dir/$file 
+    fi
+    echo "Creating new symlink to $file in neovim config directory."
+    ln -s $new_dir/$file $nvim_dir/$file
 done
+echo "...done"
 
 for file in $alacritty_files; do
-    echo "Moving any existing alacritty files from $alacritty_dir to $olddir"
+    if [ -L $alacritty_dir/$file ]; then
+        echo "Removing existing symlink ${file} from ${alacritty_dir}"
+        rm $alacritty_dir/$file
+    fi
+    echo "Creating new symlink to $file in alacritty config directory."
+    ln -s $new_dir/$file $alacritty_dir/$file
 done
+echo "...done"
 
 # zsh and oh-my-zsh setup
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # create vim .undo .swp .backup
-mkdir ~/.vim/.undo 
-mkdir ~/.vim/.swp 
-mkdir ~/.vim/.backup
+mkdir -p ~/.vim/.undo 
+mkdir -p ~/.vim/.swp 
+mkdir -p ~/.vim/.backup
